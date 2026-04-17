77483033cce65a3b6843b1a8367206438da5b522
fezeu axel <edgarfezeu@gmail.com>
2026-04-17 04:51:27 +0000

    payUnit update

--- src/lib/payUnit.ts
+++ src/lib/payUnit.ts
@@ -3,9 +3,9 @@ import { PayunitClient } from '@payunit/nodejs-sdk';
 // Initialiser avec la configuration
 export const payUnitClient = new PayunitClient({
   baseURL: 'https://gateway.payunit.net', // facultatif
-  apiKey: process.env.apiKey, //token ,live key
-  apiUsername: process.env.apiUsername,
-  apiPassword: process.env.apiPassword,
+  apiKey: process.env.PAYUNIT_apiKey, //token ,live key
+  apiUsername: process.env.PAYUNIT_apiUsername,
+  apiPassword: process.env.PAYUNIT_apiPassword,
   mode: 'live', // ou 'live',
    // facultatif
 });
